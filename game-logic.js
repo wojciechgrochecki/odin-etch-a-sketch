93dd279be8071ae250ba4c57edb0a1452de9b882
Wojciech Grochecki <wojciech5522@gmail.com>
2022-09-30 20:20:06 +0200

    clean the code

--- game-logic.js
+++ game-logic.js
@@ -1,32 +1,33 @@
 let g_rainbowMode = false;
 let g_eraseMode = false;
-let g_currentColor = "#000000";
+let g_brushColor = "#000000";
 let g_backgroundColor = "#f5f5f5";
 
 let sketchBoard = document.querySelector('.sketch-board');
 let slider = document.querySelector(".slider");
-let sliderValue = document.getElementById("slider-value");
-let brushColorPicker = document.getElementById('paint-color');
-let bgColorPicker = document.getElementById('bg-color');
+let sliderDisplayValue = document.getElementById("slider-display-value");
+let brushColor = document.getElementById('brush-color');
+let bgColor = document.getElementById('bg-color');
 let eraserButton = document.getElementById('eraser');
 let rainbowButton = document.getElementById('rainbow-mode');
 
 
-document.getElementById('save-painting').addEventListener('click', () => { storePainting(); })
+document.getElementById('save-painting').addEventListener('click', () => { savePainting(); })
 document.getElementById('paste-painting').addEventListener('click', () => { retrieveItem(); })
 
-bgColorPicker.addEventListener('input', (e) => {
+bgColor.addEventListener('input', (e) => {
     g_backgroundColor = e.target.value;
     changeBoardBackground();
 });
 
-brushColorPicker.addEventListener('change', (e) => {
-    g_currentColor = e.target.value;
+brushColor.addEventListener('input', (e) => {
+    g_brushColor = e.target.value;
 });
 
 eraserButton.addEventListener('click', (e) => {
     setModeAndClass('eraser', e.target.classList.contains('active'));
 });
+
 
 rainbowButton.addEventListener('click', (e) => {
     setModeAndClass('rainbow', e.target.classList.contains('active'));
@@ -37,11 +38,11 @@ document.getElementById('clear').addEventListener('click', () => { changeBoardBa
 document.body.ondragstart = () => { return false; };
 
 slider.addEventListener('input', (e) => {
-    sliderValue.textContent = ' ' + e.target.value + ' x ' + e.target.value;
+    sliderDisplayValue.textContent = ' ' + e.target.value + ' x ' + e.target.value;
 });
 
 slider.addEventListener('change', (e) => {
-    prepareBoard(e.target.value);
+    createBoard(e.target.value);
 });
 
 function paintDiv(e) {
@@ -51,7 +52,7 @@ function paintDiv(e) {
             e.target.style.backgroundColor = g_backgroundColor;
         }
         else {
-            e.target.style.backgroundColor = g_currentColor;
+            e.target.style.backgroundColor = g_brushColor;
         }
     }
 }
@@ -95,43 +96,58 @@ function setModeAndClass(btnName, deleteClass) {
     }
 }
 
+function savePainting() {
+    let divs = Array.from(sketchBoard.childNodes);
+}
+
 function storePainting() {
-    let clrArray = getBoardState();
-    let board = {};
-    board.size = document.querySelector('.slider').value;
-    board.colorArray = clrArray;
-    localStorage.setItem('painting', JSON.stringify(board));
-    console.log("DONE!");
+    let boardState = getBoardState();
+    localStorage.setItem('painting', JSON.stringify(boardState));
 }
 
 function getBoardState() {
+    let boardState = {};
+    boardState.colorArray = [];
     let divsArray = Array.from(sketchBoard.childNodes);
-    let colorArray = [];
     for (let i = 0; i < divsArray.length; ++i) {
-        colorArray.push(divsArray[i].style.backgroundColor);
+        boardState.colorArray.push(divsArray[i].style.backgroundColor);
     }
-    console.log(colorArray);
-    return colorArray;
+    boardState.size = Math.sqrt(divsArray.length);
+    return boardState;
 }
 
-// function retrieveItem() {
-//     let boardState = JSON.parse(localStorage.getItem('painting'));
-//     console.log(boardState.colorArray);
-//     prepareBoardFromStorage(boardState.size, boardState.colorArray);
-// }
+function retrieveItem(key = 'painting') {
+    let boardState = JSON.parse(localStorage.getItem(key));
+    createBoard(boardState.size, boardState.colorArray);
+}
 
-function prepareBoard(size) {
+
+
+function createBoard(size, colorArray) {
     sketchBoard.innerHTML = "";
     sketchBoard.style.gridTemplateColumns = "repeat(" + size + ",1fr)";
     sketchBoard.style.gridTemplateRows = "repeat(" + size + ",1fr)";
-    let items = size * size;
-    for (let i = 0; i < items; ++i) {
-        let div = document.createElement('div');
-        div.addEventListener('mouseenter', paintDiv);
-        div.addEventListener('mousedown', paintDiv);
-        div.style.backgroundColor = g_backgroundColor;
-        sketchBoard.appendChild(div);
+    sliderDisplayValue.textContent = ' ' + size + ' x ' + size;
+    slider.value = size;
+
+    let length = size * size;
+    if (colorArray === undefined)
+        for (let i = 0; i < length; ++i) {
+            let div = document.createElement('div');
+            div.addEventListener('mouseenter', paintDiv);
+            div.addEventListener('mousedown', paintDiv);
+            div.style.backgroundColor = g_backgroundColor;
+            sketchBoard.appendChild(div);
+        }
+    else {
+        for (let i = 0; i < length; ++i) {
+            let div = document.createElement('div');
+            div.addEventListener('mouseenter', paintDiv);
+            div.addEventListener('mousedown', paintDiv);
+            div.style.backgroundColor = colorArray[i];
+            sketchBoard.appendChild(div);
+        }
     }
 }
 
-prepareBoard(16);
+createBoard(16);
